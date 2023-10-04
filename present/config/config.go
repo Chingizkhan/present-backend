b4ee7f21d15bfa33f487bd09c055a436c87f421a
Chingizkhan <chingizkhan-c@auto.kz>
2023-10-04 15:09:15 +0600

    Feature:  - added postgres in pkg  - added shutdown

--- present/config/config.go
+++ present/config/config.go
@@ -10,12 +10,22 @@ type (
 	Config struct {
 		Env  string `yaml:"env" env-required:"true"`
 		HTTP `yaml:"http" env-required:"true"`
+		PG   `yaml:"postgres"`
 	}
 
 	HTTP struct {
-		Address     string        `yaml:"address" env-default:"localhost:8080"`
-		Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-		IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+		Address     string        `yaml:"address"       env-default:"localhost:8080"`
+		Timeout     time.Duration `yaml:"timeout"       env-default:"4s"`
+		IdleTimeout time.Duration `yaml:"idle_timeout"  env-default:"60s"`
+	}
+
+	PG struct {
+		PoolMax  int    `env-required:"true"     yaml:"pool_max"   env:"PG_POOL_MAX"`
+		User     string `env-required:"true"     yaml:"user"       env:"PG_USER"`
+		Password string `env-required:"true"     yaml:"password"   env:"PG_PASSWORD"`
+		Host     string `env-required:"true"     yaml:"host"       env:"PG_HOST"`
+		Port     string `env-required:"true"     yaml:"port"       env:"PG_PORT"`
+		Name     string `env-required:"true"     yaml:"name"       env:"PG_NAME"`
 	}
 )
 
--- present/internal/app/app.go
+++ present/internal/app/app.go
@@ -1,13 +1,53 @@
 package app
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"present/present/config"
+	"present/present/pkg/postgres"
+	"syscall"
 )
 
 func Run(cfg *config.Config) {
-	log.Println("config:", cfg)
 	// logger
 	// repository
+	pg, err := postgres.New(
+		postgres.GetDSN(cfg),
+		postgres.MaxPoolSize(cfg.PG.PoolMax),
+	)
+	if err != nil {
+		log.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
+	}
+	defer pg.Close()
+
 	// use case
+	// rabbitmq rpc server
+	// http server
+	// waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		log.Println("app - Run - signal: " + s.String())
+		//case s := <-interrupt:
+		//	l.Info("app - Run - signal: " + s.String())
+		//case err = <-httpServer.Notify():
+		//	l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
+		//case err = <-rmqServer.Notify():
+		//	l.Error(fmt.Errorf("app - Run - rmqServer.Notify: %w", err))
+	}
+
+	// shutdown
+	//err = httpServer.Shutdown()
+	//if err != nil {
+	//	l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+	//}
+	//
+	//err = rmqServer.Shutdown()
+	//if err != nil {
+	//	l.Error(fmt.Errorf("app - Run - rmqServer.Shutdown: %w", err))
+	//}
 }
